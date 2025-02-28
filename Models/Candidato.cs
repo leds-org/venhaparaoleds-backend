f0a886da5e579a86987944d315b28f52314388b5
Guilherme Costa <guiihocosta@gmail.com>
2025-02-28 13:44:47 -0300

    feat: Implementação do desafio backend

---
+++ Models/Candidato.cs
@@ -0,0 +1,12 @@
+﻿namespace DesafioLeds.Models
+{
+    public class Candidato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public List<string> Profissoes { get; set; } = [];
+
+    }
+}
