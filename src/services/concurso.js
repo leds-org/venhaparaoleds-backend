e40bae46d083b48f6dec9adba948c36b624c96fa
Gabriel Barbosa Lucas <gblucas180902@gmail.com>
2025-06-26 11:32:28 -0300

    feat(Concurso): Implementar métodos da camada de serviço de Concurso

--- src/services/concurso.js
+++ src/services/concurso.js
@@ -1,35 +1,106 @@
-const concursoRepos = require('../repositories/concurso');
+const concursoRepos = require('../repositories/concurso');//Import da camada de repositorio, para acessar aos métodos de interção direta com o banco de dados
+
+const concursoRepos = require('./concurso');//Import da camada de serviço de concurso (para pegar método getConcurso)
+
+//Import do módulo de criptografia
 const crypt = require('../config/criptography');
 
-async function getConcursos(limit, offset, role){
-    
-    if(limit != null && offset != null && role != null){
+async function newConcurso(data){
+    const tempo_inicial = Date.now();
+    const { codigo, edital, orgao, vagas } = data;
 
-        try{
-            const list_concursos = await concursoRepos.listConcursosByRole(limit, offset, role);
-            return {
-                sucess: true,
-                concursos: list_concursos.data
-            };
-
-        }catch(err){
-            return {
-                sucess: false,
-                message: err,
-                status_code: 400
-            }
-        }
-
-
-    }else{
+    if (!codigo || !edital || !orgao || !vagas) {
         return {
             sucess: false,
-            message: "Faltam valores a serem preenchidos.",
-            status_code: 400
+            message: "Dados do concurso incompletos.",
+            status_code: 400,
+            response_time: Date.now() - tempo_inicial
+        };
+    }
+
+    const insert_new_concurso = await concursoRepos.insertNewconcurso(data);
+
+    if(insert_new_concurso.sucess === true){
+        return {
+            sucess: true,
+            response_time: Date.now() - tempo_inicial,
         }
+    }
+    else{
+        return {
+            sucess:false,
+            message: insert_new_concurso.message,
+            status_code: insert_new_concurso.status_code,
+            response_time: Date.now() - tempo_inicial
+        }
+    }
+
+
+}
+
+async function getConcurso(id){
+    const tempo_inicial = Date.now();
+    if (!id) {
+        return {
+            sucess: false,
+            message: "Id do concurso não informado.",
+            status_code: 400,
+            response_time: Date.now() - tempo_inicial
+        };
+    }
+
+    //Chama método do repositório de concurso
+    const select_concurso_by_id = await concursoRepos.selectconcursoById(id);
+
+    if (select_concurso_by_id.sucess === true){
+        return {
+            sucess: true,
+            data: insert_new_concurso.data,
+            response_time: Date.now() - tempo_inicial
+        };
+    }
+    else{
+        return {
+            sucess: false,
+            message: insert_new_concurso.message,
+            response_time: Date.now() - tempo_inicial
+        };
     }
 }
 
+async function getConcursosCompativeis(codigo){
+    const tempo_inicial = Date.now();
+    if (!codigo) {
+        return {
+            sucess: false,
+            message: "Codigo do concurso não informado.",
+            status_code: 400,
+            response_time: Date.now() - tempo_inicial
+        };
+    }
+
+    //Chama método do repósitorio de concurso
+    const select_concursos_compativeis = await concursoRepos.selectConcursosCompativeis(codigo);
+    
+    if(select_concursos_compativeis.sucess === true){
+        return {
+            sucess: true,
+            data: select_concursos_compativeis.data,
+            response_time: Date.now() - tempo_inicial
+        };
+    }else{
+        return {
+            sucess: false,
+            message: select_concursos_compativeis.message,
+            response_time: Date.now() - tempo_inicial
+        };
+    }
+
+}
+
+
 module.exports = {
-    getConcursos,
+    newConcurso,
+    getConcurso,
+    getConcursosCompativeis,
 };
