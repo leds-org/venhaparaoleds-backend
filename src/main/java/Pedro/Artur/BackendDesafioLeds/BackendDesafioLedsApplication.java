b84968689d38420c371b8a8da588db2a6443edd3
Artur <arturcarvalho05@gmail.com>
2025-06-27 12:53:34 -0300

    Initial Commit

---
+++ src/main/java/Pedro/Artur/BackendDesafioLeds/BackendDesafioLedsApplication.java
@@ -0,0 +1,13 @@
+package Pedro.Artur.BackendDesafioLeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesafioLedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesafioLedsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Pedro/Artur/BackendDesafioLeds/BackendDesafioLedsApplicationTests.java
@@ -0,0 +1,13 @@
+package Pedro.Artur.BackendDesafioLeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesafioLedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
