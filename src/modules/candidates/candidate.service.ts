ca772118b4645292803f35e169d135a83bb85cc7
Lucas Sena <lucassenasilvamachado@gmail.com>
2025-07-02 13:19:59 -0300

    refactor: remove unnecessary try/catch

--- src/modules/candidates/candidate.service.ts
+++ src/modules/candidates/candidate.service.ts
@@ -3,25 +3,18 @@ import prisma from "../../prismaClient";
 
 // Função auxiliar para encontrar um candidato pelo CPF
 async function findCandidateByCPF(cpf: string) {
-  try {
-    // Verifica se o usuário existe no banco de dados pelo CPF
-    const existingCandidate = await prisma.candidate.findFirst({
-      where: { cpf },
-    });
+  // Verifica se o usuário existe no banco de dados pelo CPF
+  const existingCandidate = await prisma.candidate.findFirst({
+    where: { cpf },
+  });
 
-    // Se não encontrar o candidato, lança uma exceção 404
-    if (!existingCandidate) {
-      throw new HttpException(404, "Candidate not found");
-    }
-
-    // Retorna o candidato encontrado
-    return existingCandidate;
-  } catch (error) {
-    // Se o erro for uma exceção personalizada, relança
-    if (error instanceof HttpException) {
-      throw error;
-    }
+  // Se não encontrar o candidato, lança uma exceção 404
+  if (!existingCandidate) {
+    throw new HttpException(404, "Candidate not found");
   }
+
+  // Retorna o candidato encontrado
+  return existingCandidate;
 }
 
 export default class CandidateService {
@@ -31,46 +24,39 @@ export default class CandidateService {
    * @returns Lista de concursos filtrados por profissões relacionadas ao candidato
    */
   async getContestsByCPF(cpf: string) {
-    try {
-      // Busca o candidato pelo CPF
-      const candidate = await findCandidateByCPF(cpf);
+    // Busca o candidato pelo CPF
+    const candidate = await findCandidateByCPF(cpf);
 
-      // Garante que o candidato foi encontrado antes de acessar suas propriedades
-      if (!candidate) {
-        throw new HttpException(404, "Candidate not found");
-      }
-
-      // Pega e Converte as profissões do candidato de JSON para array
-      // Isso é necessário porque as profissões são armazenadas como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
-      const professions = JSON.parse(candidate.professions) as string[];
-
-      // Busca todos os concursos no banco de dados
-      const allContests = await prisma.contest.findMany();
-
-      // Filtra os concursos com base nas profissões do candidato e nos títulos de trabalho dos concursos
-      const filteredContests = allContests.filter((contest) => {
-        // Converte os títulos de trabalho do concurso de JSON para array
-        // Isso é necessário porque os títulos de trabalho são armazenados como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
-        const jobTitles = JSON.parse(contest.jobTitles) as string[];
-        // Verifica se alguma profissão do candidato está presente nos títulos de trabalho do concurso
-        // Se sim, inclui o concurso na lista filtrada
-        return professions.some((prof) => jobTitles.includes(prof));
-      });
-
-      // Mapeia os concursos filtrados para o formato desejado
-      // Retorna apenas os campos necessários: agency, publicNotice e contestCode
-      return filteredContests.map((contest) => ({
-        agency: contest.agency,
-        publicNotice: contest.publicNotice,
-        // Converte o código do concurso de BigInt para string
-        // Isso é necessário porque o código do concurso é armazenado como BigInt no banco de dados e o JSON não suporta BigInt.
-        contestCode: contest.contestCode.toString(),
-      }));
-    } catch (error) {
-      // Se o erro for uma exceção personalizada, relança
-      if (error instanceof HttpException) {
-        throw error;
-      }
+    // Garante que o candidato foi encontrado antes de acessar suas propriedades
+    if (!candidate) {
+      throw new HttpException(404, "Candidate not found");
     }
+
+    // Pega e Converte as profissões do candidato de JSON para array
+    // Isso é necessário porque as profissões são armazenadas como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
+    const professions = JSON.parse(candidate.professions) as string[];
+
+    // Busca todos os concursos no banco de dados
+    const allContests = await prisma.contest.findMany();
+
+    // Filtra os concursos com base nas profissões do candidato e nos títulos de trabalho dos concursos
+    const filteredContests = allContests.filter((contest) => {
+      // Converte os títulos de trabalho do concurso de JSON para array
+      // Isso é necessário porque os títulos de trabalho são armazenados como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
+      const jobTitles = JSON.parse(contest.jobTitles) as string[];
+      // Verifica se alguma profissão do candidato está presente nos títulos de trabalho do concurso
+      // Se sim, inclui o concurso na lista filtrada
+      return professions.some((prof) => jobTitles.includes(prof));
+    });
+
+    // Mapeia os concursos filtrados para o formato desejado
+    // Retorna apenas os campos necessários: agency, publicNotice e contestCode
+    return filteredContests.map((contest) => ({
+      agency: contest.agency,
+      publicNotice: contest.publicNotice,
+      // Converte o código do concurso de BigInt para string
+      // Isso é necessário porque o código do concurso é armazenado como BigInt no banco de dados e o JSON não suporta BigInt.
+      contestCode: contest.contestCode.toString(),
+    }));
   }
 }
--- src/modules/contests/contest.service.ts
+++ src/modules/contests/contest.service.ts
@@ -3,24 +3,17 @@ import { HttpException } from "../../utils/httpException";
 
 // funcao auxiliar para encontrar um concurso pelo código
 async function findContestByCode(code: number) {
-  try {
-    // Verifica se o concurso existe no banco de dados pelo código
-    const contest = await prisma.contest.findUnique({
-      where: { contestCode: code },
-    });
+  // Verifica se o concurso existe no banco de dados pelo código
+  const contest = await prisma.contest.findUnique({
+    where: { contestCode: code },
+  });
 
-    // Se não encontrar o concurso, lança uma exceção 404
-    if (!contest) {
-      throw new HttpException(404, "Contest not found");
-    }
-    // Retorna o concurso encontrado
-    return contest;
-  } catch (error) {
-    // Se o erro for uma exceção personalizada, relança
-    if (error instanceof HttpException) {
-      throw error;
-    }
+  // Se não encontrar o concurso, lança uma exceção 404
+  if (!contest) {
+    throw new HttpException(404, "Contest not found");
   }
+  // Retorna o concurso encontrado
+  return contest;
 }
 
 export default class ContestService {
@@ -30,37 +23,30 @@ export default class ContestService {
    * @returns Lista de candidatos filtrados por profissões relacionadas ao concurso
    */
   async getCandidatesByContestCode(code: number): Promise<any> {
-    try {
-      // Busca o concurso pelo código
-      const contest = await findContestByCode(code);
+    // Busca o concurso pelo código
+    const contest = await findContestByCode(code);
 
-      // Busca todos os candidatos no banco de dados
-      const allCandidates = await prisma.candidate.findMany();
+    // Busca todos os candidatos no banco de dados
+    const allCandidates = await prisma.candidate.findMany();
 
-      // Filtra os candidatos com base nas profissões do concurso e nos títulos de trabalho dos candidatos
-      const filteredCandidates = allCandidates.filter((candidate) => {
-        // Converte as profissões do candidato de JSON para array
-        // Isso é necessário porque as profissões são armazenadas como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
-        const professions = JSON.parse(candidate.professions) as string[];
-        // Converte os títulos de trabalho do concurso de JSON para array
-        // Isso é necessário porque os títulos de trabalho são armazenados como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
-        const jobTitles = JSON.parse(contest!.jobTitles) as string[];
-        // Verifica se alguma profissão do candidato está presente nos títulos de trabalho do concurso
-        // Se sim, inclui o candidato na lista filtrada
-        return professions.some((prof) => jobTitles.includes(prof));
-      });
-      // Mapeia os candidatos filtrados para retornar apenas os campos necessários
-      // Retorna uma lista de objetos contendo nome, CPF e data de nascimento dos candidatos filtrados
-      return filteredCandidates.map((candidate) => ({
-        name: candidate.name,
-        cpf: candidate.cpf,
-        birthdate: candidate.birthdate,
-      }));
-    } catch (error) {
-      // Se o erro for uma exceção personalizada, relança
-      if (error instanceof HttpException) {
-        throw error;
-      }
-    }
+    // Filtra os candidatos com base nas profissões do concurso e nos títulos de trabalho dos candidatos
+    const filteredCandidates = allCandidates.filter((candidate) => {
+      // Converte as profissões do candidato de JSON para array
+      // Isso é necessário porque as profissões são armazenadas como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
+      const professions = JSON.parse(candidate.professions) as string[];
+      // Converte os títulos de trabalho do concurso de JSON para array
+      // Isso é necessário porque os títulos de trabalho são armazenados como uma string JSON no banco de dados pois usei o sqlite e ele não suporta arrays
+      const jobTitles = JSON.parse(contest!.jobTitles) as string[];
+      // Verifica se alguma profissão do candidato está presente nos títulos de trabalho do concurso
+      // Se sim, inclui o candidato na lista filtrada
+      return professions.some((prof) => jobTitles.includes(prof));
+    });
+    // Mapeia os candidatos filtrados para retornar apenas os campos necessários
+    // Retorna uma lista de objetos contendo nome, CPF e data de nascimento dos candidatos filtrados
+    return filteredCandidates.map((candidate) => ({
+      name: candidate.name,
+      cpf: candidate.cpf,
+      birthdate: candidate.birthdate,
+    }));
   }
 }
