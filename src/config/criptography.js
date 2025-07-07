d962f59199ec2bc5311fed077d839625179ae9ad
Gabriel Barbosa Lucas <gblucas180902@gmail.com>
2025-07-07 13:45:26 -0300

    style: Removendo linhas de console.log do módulo de criptografia (foram usados para verificar as keys e ivs geradas)

--- src/config/criptography.js
+++ src/config/criptography.js
@@ -20,23 +20,18 @@ function criptInfo(data){
         // -> um valor "salt" (pode ser o iv);
     const key = crypto.pbkdf2Sync(CRIPT_INFOS.secret, iv, 150000, 32, 'sha256');
 
-    console.log("----DADOS DE CRIPTOGRAFIA----");
-    console.log("key: " + key.toString("hex"));
 
     //Instancia um bloco de cifragem (que é o objeto a ser usado para criptografar o dado)
     const cipher = crypto.createCipheriv(CRIPT_INFOS.algorithm, Buffer.from(key), Buffer.from(iv));
 
-    console.log("cipher: " + cipher.toString("hex"));
 
     //Criptografa o dado e atualiza bloco de cifragem já com o dado criptografado
     let cripted_data = cipher.update(data);
 
-    console.log("cripted_data: " + cripted_data.toString("hex"));
-
+    
     //Concatena os buffers do dado criptografado e do bloco de crifragem
     cripted_data = Buffer.concat([cripted_data, cipher.final()]);
-    
-    console.log("cripted_data: " + cripted_data.toString("hex"));
+
 
     /* ----------- */
     //obs.: cipher.final() fecha o bloco de cifragem, impossibilitando que novos dados sejam criptografados a partir dele
@@ -59,8 +54,6 @@ function decriptInfo(cripted_data){
     //Gera uma chave partir do mesmo iv e do mesmo segredo do dado criptografado 
     const key = crypto.pbkdf2Sync(CRIPT_INFOS.secret, iv, 150000, 32, 'sha256');
     
-    console.log("----DADOS DE DESCRIPTOGRAFIA----");
-    console.log("key: " + key.toString("hex"));
 
     /* ----------- */
     //obs.: A chave gerada, por usar os mesmos parâmetros que a mesma gerada para criptografar o dado, também será capaz de descriptografar o dado.
@@ -69,16 +62,13 @@ function decriptInfo(cripted_data){
     //Instancia um objeto para descriptografia (um decipher block)
     const decipher = crypto.createDecipheriv(CRIPT_INFOS.algorithm, Buffer.from(key), Buffer.from(iv));
     
-    console.log("decipher: " + decipher.toString("hex"));
 
     //Descriptografa o dado passado
     let decripted_data = decipher.update(encryptedData);
     
-    console.log("decripted_data: " + decripted_data.toString("hex"));
 
     decripted_data = Buffer.concat([decripted_data, decipher.final()])
-    console.log("decripted_data: " + decripted_data.toString("hex"));
-    console.log("decripted_data toString: " + decripted_data.toString("hex"));
+    
     //Retorna dado descriptografado
     return decripted_data.toString();
 
