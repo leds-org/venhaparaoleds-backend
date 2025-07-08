56fc55a15510417a3b0096a4afd5b33682603d21
Artur <arturcarvalho05@gmail.com>
2025-07-07 21:33:37 -0300

    feat: Tratamento de erros de CandidatoRepository

--- src/test/java/Pedro/Artur/BackendDesafioLeds/repository/ConcursoRepositoryTest.java
+++ src/test/java/Pedro/Artur/BackendDesafioLeds/repository/ConcursoRepositoryTest.java
@@ -22,9 +22,22 @@ class ConcursoRepositoryTest {
     EntityManager entityManager;
 
     @Test
-    @DisplayName("Deve devolver lista de concursos compativeis")
+    @DisplayName("Deve devolver lista de concursos")
     void buscarConcursosCompativeisPorProfissoesCase1() {
+        List<String> profissoes = List.of("medico","advogado");
+        this.createConcurso("SEJUS","17/2024",95655123539L,profissoes);
+        List<Concurso> result = concursoRepository.buscarConcursosCompativeisPorProfissoes(profissoes);
+        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(1);
+        assertThat(result).extracting(Concurso::getEdital).containsExactly("17/2024");
+    }
 
+    @Test
+    @DisplayName("Não deve devolver lista de concursos compativeis quando nao existir")
+    void buscarConcursosCompativeisPorProfissoesCase2() {
+        Long codigo_invalido = 95655123539L;
+        List<Concurso> result = concursoRepository.findByCodigo(codigo_invalido);
+        assertThat(result).isEmpty();
     }
 
     @Test
@@ -32,21 +45,19 @@ class ConcursoRepositoryTest {
     void findByCodigoCase1() {
         Long codigo_valido = 95655123539L;
         this.createConcurso("SEJUS","17/2024",codigo_valido,List.of("medico","advogado"));
+        this.createConcurso("SEJUS","12/2021",codigo_valido,List.of("enfermeiro","marceneiro"));
         List<Concurso> result = concursoRepository.findByCodigo(codigo_valido);
-        assertThat(result.isEmpty()).isFalse();
+        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(2);
+        assertThat(result).extracting(Concurso::getEdital).containsExactlyInAnyOrder("12/2021","17/2024");
     }
 
     @Test
-    @DisplayName("Não deve devolver lista de concursos compativeis")
-    void buscarConcursosCompativeisPorProfissoesCase2() {
-    }
-
-    @Test
-    @DisplayName("Não deve devolver lista de concursos")
+    @DisplayName("Não deve devolver lista de concursos quando codigo nao existe")
     void findByCodigoCase2() {
         Long codigo_invalido = 12345678912L;
         List<Concurso> result = concursoRepository.findByCodigo(codigo_invalido);
-        assertThat(result.isEmpty()).isTrue();
+        assertThat(result).isEmpty();
     }
 
     private Concurso createConcurso(String orgao, String edital, Long codigo, List<String> profissoes){
